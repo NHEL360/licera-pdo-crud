1bdbd5bc7355d6821c76771c42ad3ff55d060116
NHEL360 <nhelmeynard@gmail.com>
2024-04-22 17:52:49 +0800

    adding db product.sql

--- db/config.php
+++ db/config.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_USERNAME', '');
 define('DB_SERVER', 'localhost');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'licera');
- 
+define('DB_NAME', 'u593341949_db_serenio');
+define('DB_USERNAME', 'u593341949_dev_serenio');
+define('DB_PASSWORD', '20212014Serenio');
+
 try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
